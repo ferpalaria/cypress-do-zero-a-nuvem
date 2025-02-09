64eff4aca4f4397629920a87795b9ceac27d3649
Fernanda <fernanda.palaria.ctr@brightside.com>
2025-02-08 22:10:13 -0500

    fix: asserting expecting wrong name

--- cypress/e2e/cac-tat.cy.js
+++ cypress/e2e/cac-tat.cy.js
@@ -107,7 +107,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
     cy.contains('a', 'Política de Privacidade')
       .should('have.attr', 'target', '_blank')
-      .and('have.attr', 'href', 'privacidade.html')
+      .and('have.attr', 'href', 'privacy.html')
   })
 
   it('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
