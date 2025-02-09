4d5e8c69e75fb8cab82831ba5fb82f383bb6fc2a
Fernanda <fernanda.palaria.ctr@brightside.com>
2025-02-08 23:36:09 -0500

    feat: config cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {},
   viewportHeight: 880,
-  viewportWidth: 1280
+  viewportWidth: 1280,
+  projectId: "fyqcdg",
 });
 
 // {
